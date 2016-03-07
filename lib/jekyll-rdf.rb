af022941ccf25fde35a1dcc8f1c00bd6705ad21d
Simon Jakobi <simon.jakobi@gmail.com>
2016-03-07 10:26:06 +0100

    Try to please rubocop

--- lib/jekyll-rdf.rb
+++ lib/jekyll-rdf.rb
@@ -2,28 +2,23 @@ require 'linkeddata'
 require 'sparql'
 
 module JekyllRdf
-
   class Generator < Jekyll::Generator
-      safe true
-      priority :highest
+    safe true
+    priority :highest
 
-      def generate(site)
-        config = site.config['jekyll_rdf']
-        if !config
-          return
-        end
+    def generate(site)
+      config = site.config['jekyll_rdf']
+      return unless config
 
-        queryable = RDF::Repository.load(config['path'], format: :ttl)
-        site.data['rdf'] = []
-        queryable.statements.each do |statement|
-          site.data['rdf'] << [
-            statement.subject.to_s,
-            statement.predicate.to_s,
-            statement.object.to_s
-          ]
-        end
+      queryable = RDF::Repository.load(config['path'], format: :ttl)
+      site.data['rdf'] = []
+      queryable.statements.each do |statement|
+        site.data['rdf'] << [
+          statement.subject.to_s,
+          statement.predicate.to_s,
+          statement.object.to_s
+        ]
       end
-
-   end
-
+    end
+  end
 end
