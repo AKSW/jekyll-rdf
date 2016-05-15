5a38407c8041c9ca5b51ab9436566299d9285d0a
riesenrad <none@none>
2016-05-15 20:33:46 +0200

    Improve documentation of Jekyll::RdfMainGenerator#extract_resources

--- lib/jekyll/rdf_main_generator.rb
+++ lib/jekyll/rdf_main_generator.rb
@@ -34,15 +34,30 @@ module Jekyll
     end
 
     ##
-    # #extract_resources restricts the RDF graph with given restriction arguments
+    # #extract_resources returns resources from an RDF graph.
+    # 
+    # Literals are omitted.
+    # Blank nodes are only returned if +include_blank+ is true.
+    # Duplicate nodes are removed.
     #
-    # * +restriction+ - The option you want to restrict the RDF graph with - only subjects, objects, predicates or custom SPARQL query
+    # * +selection+ - choose any of the following:
+    #   nil ::
+    #     no restrictions, return subjects, predicates, objects 
+    #   "subjects" ::
+    #     return only subjects
+    #   "predicates" ::
+    #     return only predicates
+    #   "objects" ::
+    #     return only objects
+    #   Otherwise ::
+    #     consider +selection+ to be a SPARQL query and return answer set to this SPARQL query
+    # * +include_blank+ - If true, blank nodes are also returned, otherwise blank nodes are omitted
     # * +graph+ - The RDF graph to restrict
-    # * +sparql+ - The SPARQL client to run queries
+    # * +sparql+ - The SPARQL client to run queries against
     #
-    def extract_resources(restriction, include_blank, graph, sparql)
+    def extract_resources(selection, include_blank, graph, sparql)
 
-      case restriction
+      case selection
       when nil  # Config parameter not present
         object_resources    = extract_resources("objects",    include_blank, graph, sparql)
         subject_resources   = extract_resources("subjects",   include_blank, graph, sparql)
@@ -56,7 +71,7 @@ module Jekyll
         graph.predicates
       else
         # Custom query
-        sparql.query(restriction).map{|sol| sol.each_value.first}
+        sparql.query(selection).map{|sol| sol.each_value.first}
       end.reject do |s|  # Reject literals
         s.class <= RDF::Literal 
       end.select do |s|  # Select URIs and blank nodes in case of include_blank
