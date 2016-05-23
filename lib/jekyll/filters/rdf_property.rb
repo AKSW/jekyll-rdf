d1fe23bc653849e4f6396f1e0ee277f147f39e99
frommie <christian.frommert@gmail.com>
2016-05-23 21:03:37 +0200

    Implement language-selection for single property filter

--- lib/jekyll/filters/rdf_property.rb
+++ lib/jekyll/filters/rdf_property.rb
@@ -1,10 +1,15 @@
 module Jekyll
   module RdfProperty
 
-    def rdf_property(input, predicate)
+    def rdf_property(input, params)
       return input unless input.is_a?(Jekyll::Drops::RdfResource)
       begin
-        result = input.page.data['rdf'].statements_as_subject.select{ |s| s.predicate.term.to_s == predicate }
+        params = params.split(",")
+        results = input.page.data['rdf'].statements_as_subject.select{ |s| s.predicate.term.to_s == params[0] }
+        lang = params[1] || input.site.config['language']
+        if results.count > 1 && results.first.object.term.is_a?(RDF::Term)
+          p = results.find{ |s| s.object.term.language == lang.to_sym }
+        end
         p = result.first unless p
         return unless p
         (p.object.name).to_s
