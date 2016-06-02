738f2348852ff81ae98168469f4d1f00845b7b31
frommie <christian.frommert@gmail.com>
2016-06-02 10:37:33 +0200

    Implement language selection feature for property lists

--- lib/jekyll/filters/rdf_property_list.rb
+++ lib/jekyll/filters/rdf_property_list.rb
@@ -1,7 +1,7 @@
 module Jekyll
-  
+
   ##
-  # Internal module to hold the medthod #rdf_property
+  # Internal module to hold the method #rdf_property
   #
   module RdfPropertyList
 
@@ -9,14 +9,21 @@ module Jekyll
     # Computes all objects for which statements exist containing the given subject and predicate and returns an Array of them
     #
     # * +input+ - is the subject of the statements to be matched
-    # * +property+ - is the predicate of the statements to be matched
+    # * +predicate+ - is the predicate of the statements to be matched
+    # * +lang+ - (optional) preferred language of the returned objects. If 'cfg' is specified the preferred language is provides by the site configuration _config.yml
     #
-    def rdf_property_list(input, property)
+    def rdf_property_list(input, predicate, lang = nil)
       return input unless input.is_a?(Jekyll::Drops::RdfResource)
       begin
-        result = input.page.data['rdf'].statements_as_subject.select{ |s| s.predicate.term.to_s == property }.map{|p| p.object.name}
+        result = input.page.data['rdf'].statements_as_subject.select{ |s| s.predicate.term.to_s == predicate } # select all matching statements with given predicate
+        if lang != nil
+          if lang == 'cfg'
+            lang ||= input.site.config['language']
+          end
+          result = result.select{ |s| s.object.term.language == lang.to_sym } # select all statements with matching language
+        end
         return unless result
-        result
+        result.map{|p| p.object.name}
       end
     end
 
