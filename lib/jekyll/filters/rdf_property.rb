599ee4aa05c8668120e757e85593510776d6f38d
frommie <christian.frommert@gmail.com>
2016-06-02 11:31:59 +0200

    Fix language selection when language is provided by configuration

--- lib/jekyll/filters/rdf_property.rb
+++ lib/jekyll/filters/rdf_property.rb
@@ -1,10 +1,10 @@
 module Jekyll
-  
+
   ##
   # Internal module to hold the medthod #rdf_property
   #
   module RdfProperty
-  	  
+
     ##
     # Computes all objects for which statements exist containing the given subject and predicate and returns any of them
     #
@@ -16,7 +16,7 @@ module Jekyll
       return input unless input.is_a?(Jekyll::Drops::RdfResource)
       begin
         results = input.page.data['rdf'].statements_as_subject.select{ |s| s.predicate.term.to_s == predicate }
-        lang ||= input.site.config['language']
+        lang ||= input.site.config['jekyll_rdf']['language']
         if results.count > 1 && results.first.object.term.is_a?(RDF::Term) && lang != nil
           p = results.find{ |s| s.object.term.language == lang.to_sym }
         end
--- lib/jekyll/filters/rdf_property_list.rb
+++ lib/jekyll/filters/rdf_property_list.rb
@@ -18,7 +18,7 @@ module Jekyll
         result = input.page.data['rdf'].statements_as_subject.select{ |s| s.predicate.term.to_s == predicate } # select all matching statements with given predicate
         if lang != nil
           if lang == 'cfg'
-            lang ||= input.site.config['language']
+            lang = input.site.config['jekyll_rdf']['language']
           end
           result = result.select{ |s| s.object.term.language == lang.to_sym } # select all statements with matching language
         end
