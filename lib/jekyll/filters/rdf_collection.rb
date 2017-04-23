639e15e7a83eb6b9244e931fb96f5991f7a90474
Nime <sebastianz541@googlemail.com>
2017-04-23 15:50:04 +0200

    test tweaks so they do not fail

--- lib/jekyll/filters/rdf_collection.rb
+++ lib/jekyll/filters/rdf_collection.rb
@@ -1,7 +1,6 @@
 module Jekyll
   module RdfCollection
     def rdf_collection(input)
-      Jekyll.logger.warn "enter rdf_collection"
       if(input.statements_as_subject.any?{ |statement|
         statement.predicate.to_s.eql? "http://www.w3.org/1999/02/22-rdf-syntax-ns#rest"
       })
@@ -12,7 +11,6 @@ module Jekyll
         while(!nodeSequence.empty?)
           currentContainer = nodeSequence.pop
           query = "SELECT ?f ?r WHERE{ <#{currentContainer}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest> ?r. <#{currentContainer}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#first> ?f}"
-          Jekyll.logger.warn query
           solutions = sparqlClient.query(query).each {|solution|
             if((!solution.r.to_s.eql? "http://www.w3.org/1999/02/22-rdf-syntax-ns#nil") && (finalizedContainer.add? solution.r))
               nodeSequence.push solution.r.to_s
@@ -20,7 +18,6 @@ module Jekyll
             results.push Jekyll::Drops::RdfTerm.build_term_drop(solution.f, input.graph, input.site)
           }
           end
-        pp results
         return results
       end
     end
