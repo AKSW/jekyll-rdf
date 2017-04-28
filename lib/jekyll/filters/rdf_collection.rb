822088e0f0304607ec5c99da4b48e7a868844880
Nime <sebastianz541@googlemail.com>
2017-04-28 13:39:09 +0200

    made rdf_collection hierarchical
    rdf_collection does not return solutions out of order. if solutions a and b are target of cs rdf:rest, rdf_collection returns c before a or b

--- lib/jekyll/filters/rdf_collection.rb
+++ lib/jekyll/filters/rdf_collection.rb
@@ -6,16 +6,21 @@ module Jekyll
       })
         finalizedContainer = Set.new #avoid loops
         nodeSequence = [input.iri]
+        nextSequence = []
         results = []
         sparqlClient = input.site.data['sparql']
         while(!nodeSequence.empty?)
           currentContainer = nodeSequence.pop
           query = "SELECT ?f ?r WHERE{ <#{currentContainer}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#rest> ?r. <#{currentContainer}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#first> ?f}"
-          solutions = sparqlClient.query(query).each {|solution|
+          solutions = sparqlClient.query(query).each { |solution|
             if((!solution.r.to_s.eql? "http://www.w3.org/1999/02/22-rdf-syntax-ns#nil") && (finalizedContainer.add? solution.r))
-              nodeSequence.push solution.r.to_s
+              nextSequence.push solution.r.to_s
             end
             results.push Jekyll::Drops::RdfTerm.build_term_drop(solution.f, input.graph, input.site)
+            if(nodeSequence.empty?)
+              nodeSequence = nextSequence
+              nextSequence = []
+            end
           }
           end
         return results
