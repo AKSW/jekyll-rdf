cc3274c8aaef20a783f70b4c1f5e4a5f63b9633f
Nime <sebastianz541@googlemail.com>
2018-11-12 16:36:53 +0100

    rename rdf_message -> rdf_debug_message

--- lib/jekyll/filters/rdf_debug_message.rb
+++ lib/jekyll/filters/rdf_debug_message.rb
@@ -6,7 +6,7 @@ module Jekyll
     # Internal module to hold the medthod #rdf_message
     #
     module Filter
-      def rdf_message(message, logLevel = "info") #:nodoc:
+      def rdf_debug_message(message, logLevel = "info") #:nodoc:
         case logLevel
         when "info"
           Jekyll.logger.info message
