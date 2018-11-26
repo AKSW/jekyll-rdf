325d66fe39dea86bced04617580b8aee321c08b9
Nime <sebastianz541@googlemail.com>
2018-11-26 20:44:16 +0100

    tests for reduced warnings

--- test/cases/multipleWarnings/test_multiple_warnings.rb
+++ test/cases/multipleWarnings/test_multiple_warnings.rb
@@ -3,12 +3,17 @@ class TestGeneral < Test::Unit::TestCase
   include RdfTestUtility
   context "Jekyll-Rdf template mapping" do
     setup do
+      std_dummy = $stderr
+      $stderr = StringIO.new
+      log_level = Jekyll.logger.level
+      Jekyll.logger.log_level = :warn
       setup_jekyll File.dirname(__FILE__)
+      Jekyll.logger.log_level = log_level
+      $stderr = std_dummy
     end
 
     should "send warnings only once per " do
-      assert Jekyll.logger.messages.any? {|message|
-        Jekyll.logger.error "#{(message.include? "Warning: multiple possible templates for resources")} #{(message.include? "http://example.org/instance/main4")} #{(message.include? "http://example.org/instance/main3")} #{(message.include? "Possible Templates:")} #{(message.include? "layout9")} #{(message.include? "layout10")} #{(message.include? "layout11")} #{(message.include? "layout12")}"
+      assert  Jekyll.logger.messages.any? {|message|
         (message.include? "Warning: multiple possible templates for resources")&&
         (message.include? "http://example.org/instance/main4")&&
         (message.include? "http://example.org/instance/main3")&&
